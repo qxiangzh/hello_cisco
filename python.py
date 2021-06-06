e6502203ff0e5ecc29b5532b5a6e09da690646b4
qxiangzh <qxiangzh@gmail.com>
2021-06-06 15:53:20 +0800

    final

--- python.py
+++ python.py
@@ -3,14 +3,11 @@ import sys
 print sys.path
 print sys.platform
 
-<<<<<<< HEAD
-sum = a + b;
-primt(sum)
-=======
+
 def result(a, b)
     sum = a + b
     print(sum)
     return sum
->>>>>>> 2nd_branch
+
 
 testing
